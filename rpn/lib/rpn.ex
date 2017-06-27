e5c80ed425b14e1e2eb4417ff897781d764cd7a1
Vincent <vinh.nglx@gmail.com>
2017-06-27 14:43:55 +0800

    Push an operand to stack and the data currently from the calculation

--- rpn/lib/rpn.ex
+++ rpn/lib/rpn.ex
@@ -13,4 +13,30 @@ defmodule Rpn do
   def start do
     Agent.start(fn -> [] end)
   end
+
+  @doc """
+    Push an operand to stack
+
+    ## Example
+
+      iex > {:ok, pid} = Rpn.start
+      iex > Rpn.push(pid, 5)
+  """
+  def push(pid, operand) do
+    Agent.update(pid, fn(list)-> [operand|list] end)
+  end
+
+  @doc """
+    Get the data currently of the calculation
+
+    ## Example
+
+      iex > {:ok, pid} = Rpn.start
+      iex > Rpn.push(pid, 5)
+      iex > Rpn.peek(pid)
+      # [5]
+  """
+  def peek(pid) do
+    Agent.get(pid, fn(list)-> list end)
+  end
 end
--- rpn/test/rpn_test.exs
+++ rpn/test/rpn_test.exs
@@ -3,9 +3,16 @@ defmodule RpnTest do
   doctest Rpn
 
   # Rpn's methods
+
   test "start a Rpn calculation" do
     {:ok, pid} = Rpn.start
     assert is_pid(pid) == true
+    assert Rpn.peek(pid) == []
   end
 
+  test "push an operand to stack" do
+    {:ok, pid} = Rpn.start
+    Rpn.push(pid, 5)
+    assert Rpn.peek(pid) == [5]
+  end
 end
