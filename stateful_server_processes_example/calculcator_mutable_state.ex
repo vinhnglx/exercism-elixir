294282776a735631f45bc22bb03c9503c0cbb719
Vincent <vinh.nglx@gmail.com>
2017-07-04 21:42:17 +0800

    Extract cases inside loop function to multiple private condition-functions

--- stateful_server_processes_example/calculcator_mutable_state.ex
+++ stateful_server_processes_example/calculcator_mutable_state.ex
@@ -1,6 +1,8 @@
 defmodule Calculator do
-  def start do
-    spawn(fn -> loop(0) end)
+  def start(initial_state) do
+    # initial_state is 0 if start a process for add/sub
+    # initial_state is 1 if start a process for mul
+    spawn(fn -> loop(initial_state) end)
   end
 
   def value(server_pid) do
@@ -17,19 +19,33 @@ defmodule Calculator do
 
   defp loop(current_value) do
     new_value = receive do
-      {:add, value} -> current_value + value
-      {:sub, value} -> current_value - value
-      {:mul, value} -> current_value * value
-      {:div, value} -> current_value / value
-      {:value, caller} ->
-        send(caller, {:response, current_value})
-        current_value
+      message -> process_message(current_value, message)
+      # {:add, value} -> current_value + value
+      # {:sub, value} -> current_value - value
+      # {:mul, value} -> current_value * value
+      # {:div, value} -> current_value / value
+      # {:value, caller} ->
+      #   send(caller, {:response, current_value})
+      #   current_value
 
-      invalid_request ->
-        IO.puts "invalid request #{inspect invalid_request}"
-        current_value
+      # invalid_request ->
+      #   IO.puts "invalid request #{inspect invalid_request}"
+      #   current_value
     end
 
     loop(new_value)
+  end
+
+  defp process_message(current_value, {:add, value}), do: current_value + value
+  defp process_message(current_value, {:sub, value}), do: current_value - value
+  defp process_message(current_value, {:mul, value}), do: current_value * value
+  defp process_message(current_value, {:div, value}), do: current_value / value
+  defp process_message(current_value, {:value, caller}) do
+    send(caller, {:response, current_value})
+    current_value
+  end
+  defp process_message(current_value, invalid_request) do
+    IO.puts "invalid request #{inspect invalid_request}"
+    current_value
   end
 end
