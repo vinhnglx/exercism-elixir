29ae72b64301f1dd9714a814bc21cabcfbabd0c7
Vincent <vinh.nglx@gmail.com>
2017-07-05 17:48:30 +0800

    Using GenServer instead for Ceto - ServerProcess

--- kito/lib/kito.ex
+++ kito/lib/kito.ex
@@ -1,33 +1,28 @@
 defmodule Kito do
-  def init do
-    Map.new
+  use GenServer
+
+  def init(_) do
+    {:ok, Map.new}
   end
 
-  def handle_call({:put, key, value}, state) do
-    # {response, new_state}
-    {:ok, Map.put(state, key, value)}
-  end
-
-  def handle_call({:get, key}, state) do
-    # {response, new_state}
-    {Map.get(state, key), state}
+  def handle_call({:get, key}, _, state) do
+    {:reply, Map.get(state, key), state}
   end
 
   def handle_cast({:put, key, value}, state) do
-    Map.put(state, key, value)
+    {:noreply, Map.put(state, key, value)}
   end
 
   # Interface functions
   def start do
-    Ceto.start(Kito)
+    GenServer.start(Kito, nil)
   end
 
   def put(pid, key, value) do
-    # Ceto.call(pid, {:put, key, value})
-    Ceto.cast(pid, {:put, key, value})
+    GenServer.cast(pid, {:put, key, value})
   end
 
   def get(pid, key) do
-    Ceto.call(pid, {:get, key})
+    GenServer.call(pid, {:get, key})
   end
 end
--- kito/test/kito_test.exs
+++ kito/test/kito_test.exs
@@ -3,17 +3,17 @@ defmodule KitoTest do
   doctest Kito
 
   test "starts a new kito process" do
-    assert is_pid(Kito.start) == true
+    {:ok, pid} = Kito.start
+    assert is_pid(pid) == true
   end
 
   test "put a new key value to kito" do
-    pid = Kito.start
-    # assert Kito.put(pid, :first_name, "vincent") == :ok
-    assert Kito.put(pid, :first_name, "vincent") == {:cast, {:put, :first_name, "vincent"}}
+    {:ok, pid} = Kito.start
+    assert Kito.put(pid, :first_name, "vincent") == :ok
   end
 
   test "get a existing value from a key" do
-    pid = Kito.start
+    {:ok, pid} = Kito.start
     Kito.put(pid, :first_name, "vincent")
     assert Kito.get(pid, :first_name) == "vincent"
   end
